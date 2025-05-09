ecf360c8c55e2675ce15b676a0048984739aa51c
cyybxrg Sourabh <cyborgop6@gmail.com>
2025-05-10 01:46:15 +0530

    fixed all the issues and apis working fine

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,13 +1,9 @@
 import { HamBurgerMenu } from "./HamburgerMenu";
-import { ModeToggle } from "./mode-toggle";
 import { Button } from "./ui/button";
 
 
-type tailwindProps = {
-    className: string;
-}
 
-export const NavBar = ({className}:tailwindProps)=>{
+export const NavBar = ()=>{
 
 
 
@@ -16,30 +12,29 @@ export const NavBar = ({className}:tailwindProps)=>{
   
     return (
       <div
-        className={`flex sticky py-2 border transparent px-4 gap-4 justify-between backdrop-blur-3xl ${className}`}
+        className={`flex sticky fixed top-0 py-2 border transparent px-4 gap-4 justify-between backdrop-blur-3xl `}
       >
-        <div className="font-GTWalsheim text-2xl sm:text-4xl text-purple-500 text-nowrap">
+        <div className="font-GTWalsheim text-2xl sm:text-4xl text-purple-500 text-nowrap ">
           FairSplit
         </div>
 
         <ul className="hidden font-mono md:flex items-center gap-4 text-nowrap text-lg ">
-          <li className="text-purple-500">
-            {" "}
-            <a href="#">Pricing</a>
-          </li>
-          <li>
+          <li className="border rounded-4xl p-2 hover:bg-gray-900">
             {" "}
             <a href="#">About Us</a>
           </li>
-          <li>
+          <li className="border rounded-4xl p-2 hover:bg-gray-900">
+            {" "}
+            <a href="#">Pricing</a>
+          </li>
+          <li className="border rounded-4xl p-2 hover:bg-gray-900">
             {" "}
             <a href="#">Contact Us</a>
           </li>
-          
         </ul>
 
         <div className="flex gap-6 font-GTWalsheim">
-          <Button>SignUp</Button>
+          <Button className="hidden sm:block">SignUp</Button>
           <Button>Login</Button>
           <HamBurgerMenu />
         </div>
